6c5add98987d28eb938d3a26066437d98ea7d888
LS-KS <116895972+LS-KS@users.noreply.github.com>
2023-06-16 13:47:43 +0200

    Mehr Text

--- LaTeX/Listings/Demo1.py
+++ LaTeX/Listings/Demo1.py
@@ -1,24 +1,20 @@
-if __name__ == "__main__":
-    '''Create Basic Application Class and QMLEngine'''
-    app = QGuiApplication(sys.argv)
-    engine = QQmlApplicationEngine()
+'''Create Basic Application Class and QMLEngine'''
+app = QGuiApplication(sys.argv)
+engine = QQmlApplicationEngine()
 
-    # Create simple Productlistodel from path to database file
-    productListModel = ProductListModel(getProducts(PRODUCTLIST))
-    engine.rootContext().setContextProperty("productListModel",
-                                            productListModel)
+# Create simple Productlistodel from path to database file
+listModel = ProductListModel(getProducts(PRODUCTLIST))
+engine.rootContext().setContextProperty("listModel",listModel)
 
-    # create InventoryController instance
-    productListController = ProductListController(productlist=
-                                                  productListModel)
-    engine.rootContext().setContextProperty("productListController",
-                                            productListController)
+# create InventoryController instance
+listController = ProductListController(listModel)
+engine.rootContext().setContextProperty("listCon",listController)
 
-    # define load main.qml file to start application
-    qml_file = Path(__file__).resolve().parent / "src" / "qml" / "main.qml"
-    engine.load(qml_file)
+# define load main.qml file to start application
+qml_file = "../src/qml/main.qml"
+engine.load(qml_file)
 
-    if not engine.rootObjects():
-        sys.exit(-1)
+if not engine.rootObjects():
+    sys.exit(-1)
 
-    sys.exit(app.exec())
+sys.exit(app.exec())
--- LaTeX/Semesterarbeit_Lennart_Schink.lol
+++ LaTeX/Semesterarbeit_Lennart_Schink.lol
@@ -1,7 +1,8 @@
 \addvspace {10\p@ }
 \addvspace {10\p@ }
 \addvspace {10\p@ }
-\contentsline {lstlisting}{Listings/Demo1.py}{24}{lstlisting.3.-1}%
+\contentsline {lstlisting}{\numberline {3.1}{\ignorespaces Beispiel einer einfachen App mittels PySide6. \small \add@extra@listi {sml}Zunächst wird eine Instanz der Application-Klasse und der QmlEngine erzeugt. Danach werden Objekte eines Datenmodells und eines Controllers erzeugt und als rootContext der QmlEngine registriert. Anschließend wird die QML Datei des Hauptfensters geladen, was die App startet.}}{27}{lstlisting.3.1}%
+\contentsline {lstlisting}{\numberline {3.2}{\ignorespaces Beispiel einer QML Datei\small \add@extra@listi {sml}Diese QML Datei erzeugt ListView QML Type, der zum Anzeigen der Daten in einem ListModel verwendet wird. Innerhalb eines Rechtecks mit farbigem Rand werden in einem RowLayout Type die Datensätze des Datenmodells gerendert und über die Funktionslogik von Signalen der QML Types und der Controllerklasse farblioch markiert.}}{27}{lstlisting.3.2}%
 \addvspace {10\p@ }
 \addvspace {10\p@ }
 \addvspace {10\p@ }
