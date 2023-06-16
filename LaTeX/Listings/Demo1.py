be055de31863aef32c65cef7f4bddbf6468c04c2
LS-KS <116895972+LS-KS@users.noreply.github.com>
2023-06-16 10:39:38 +0200

    wechsel

--- LaTeX/Listings/Demo1.py
+++ LaTeX/Listings/Demo1.py
@@ -1,3 +1,24 @@
-# This program prints Hello, world!
+if __name__ == "__main__":
+    '''Create Basic Application Class and QMLEngine'''
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
 
-print('Hello, world!')
+    # Create simple Productlistodel from path to database file
+    productListModel = ProductListModel(getProducts(PRODUCTLIST))
+    engine.rootContext().setContextProperty("productListModel",
+                                            productListModel)
+
+    # create InventoryController instance
+    productListController = ProductListController(productlist=
+                                                  productListModel)
+    engine.rootContext().setContextProperty("productListController",
+                                            productListController)
+
+    # define load main.qml file to start application
+    qml_file = Path(__file__).resolve().parent / "src" / "qml" / "main.qml"
+    engine.load(qml_file)
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    sys.exit(app.exec())
--- LaTeX/Semesterarbeit_Lennart_Schink.lol
+++ LaTeX/Semesterarbeit_Lennart_Schink.lol
@@ -1,6 +1,7 @@
 \addvspace {10\p@ }
 \addvspace {10\p@ }
 \addvspace {10\p@ }
+\contentsline {lstlisting}{Listings/Demo1.py}{24}{lstlisting.3.-1}%
 \addvspace {10\p@ }
 \addvspace {10\p@ }
 \addvspace {10\p@ }
